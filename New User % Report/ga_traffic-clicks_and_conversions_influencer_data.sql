ed0e2fc4eb676258a349a266e372dd6cf08ac85b
Nikita Gupta <68778711+nikitaguptaa@users.noreply.github.com>
2025-09-16 01:54:21 +0530

    Create ga_traffic-clicks_and_conversions_influencer_data.sql

---
+++ New User % Report/ga_traffic-clicks_and_conversions_influencer_data.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `ltpautomatedpublisherscorecard.ga_traffic.first_last_conversions` where Publisher_ID 
+in(2975314,687298,116548,73861,2575853,
+3096943,57486,1909792,136659,86125,
+4511941,52269,92415,
+1390754, 5993667, 189995)
